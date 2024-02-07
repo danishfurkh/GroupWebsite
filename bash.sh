5fc1b53944df00a22fcfe99ba4538de56ea6ae0d
Danish Furekh Dar <68053957+danishfurkh@users.noreply.github.com>
2024-02-07 13:59:18 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Check for Ruby
+if ! command -v ruby &> /dev/null
+then
+    echo "Ruby could not be found. Installing Ruby..."
+    sudo apt-get install ruby-full
+    # On macOS with Homebrew, you might use: brew install ruby
+    # Adjust the installation command according to your OS or exit the script
+    exit 1
+fi
+
+# Check for Jekyll
+if ! gem list jekyll -i > /dev/null 2>&1
+then
+    echo "Jekyll could not be found. Installing Jekyll..."
+    gem install jekyll bundler
+fi
+
 # Echo commands
 echo "Running Ruby code..."
 ruby bib_to_json.rb
@@ -9,12 +26,12 @@ bundle update
 jekyll build
 
 # Uncomment the following lines if you need to clear the screen
-# clear
+clear
 
 # Uncomment the following lines if you want to prompt for username and upload files using scp
- echo "Enter username and password to upload files to Website"
- read -p "Enter your username: " username
- scp -r _site/* "$username"@webserv3.rz.uni-jena.de:web/
+echo "Enter username and password to upload files to Website"
+read -p "Enter your username: " username
+scp -r _site/* "$username"@webserv3.rz.uni-jena.de:web/
 
 # Static username version
 # scp -r _site/* atthdanishfurkh@webserv3.rz.uni-jena.de:web/
